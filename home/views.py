df753edad22c0ea45835347b269610d83cafd3a8
Aluno <you@example.com>
2022-10-25 16:45:36 -0300

    Update files

--- home/views.py
+++ home/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        'disciplina': 'Desenvolvimento Web - Técnico em Informática - Politécnico UFSM',
+        'tecnologia': 'Python e Django',
+    }
+
+    return render(request, 'index.html', context)
