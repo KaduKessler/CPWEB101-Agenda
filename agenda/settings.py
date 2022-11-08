4d4a17b0bb36450ae45d0ef1a99a775f75fe5a46
Aluno <you@example.com>
2022-11-08 16:23:20 -0300

    add cliente & funcionario

--- agenda/settings.py
+++ agenda/settings.py
@@ -41,8 +41,14 @@ INSTALLED_APPS = [
 ]
 
 INSTALLED_APPS += [
+    'stdimage',
+]
+
+INSTALLED_APPS += [
     'home',
     'produto',
+    'cliente',
+    'funcionario',
 ]
 
 MIDDLEWARE = [
@@ -122,12 +128,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
---
+++ cliente/admin.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.utils.html import format_html
+
+from .models import Cliente
+# Register your models here.
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    fields = ('nome', 'endereco', 'fone', 'email', 'foto', 'fotografia')
+    list_display = ('nome', 'fone', 'email', 'endereco')
+    readonly_fields = ['fotografia']
+
+    def fotografia(self, obj):
+        if obj.foto:
+            return format_html('<img width="150px" src="{}" />', obj.foto.url)
+        pass
